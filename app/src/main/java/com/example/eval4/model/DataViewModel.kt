40a2f624af67d4685894b7297d65be0331e22519
joelvivandsouza <joelviviandsouza2001@gmail.com>
2023-09-11 19:25:48 +0530

    Performed changes

---
+++ app/src/main/java/com/example/eval4/model/DataViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.eval4.model
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class DataViewModel : ViewModel() {
+
+    private var _offersList = listOf<Item>(
+        Item(text =  "100 Credits Prize/Ticket Game Card"),
+        Item( text =  "Card with coke"),
+        Item( text =  "New Birthday Card"),
+        Item( text =  "Birthday game card with Discount"),
+        Item( text =  "Birthday game card with 10% discount")
+    )
+
+    val offersList get() = _offersList
+
+    val offersSelected get() = _offersList.filter { it.selected }
+
+    private var _offersSelectedSize = MutableLiveData<Int>(0)
+    val offersSelectedSize get() = _offersSelectedSize
+
+
+    fun toggleSelect(pos : Int){
+        _offersList[pos].selected = !_offersList[pos].selected
+        updateSize()
+    }
+
+    fun updateSize(){
+        _offersSelectedSize.value = offersSelected.size
+    }
+
+}
